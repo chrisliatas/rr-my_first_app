022d58828fe6374e518b807ddd04b6c7dc6d6a37
classhopper <classhopp3r@gmail.com>
2018-01-22 12:19:57 +0200

    Made changes according to lectures but the Coursera API is not working, so cannot test it

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,6 +19,9 @@ Rails.application.configure do
   # `config/secrets.yml.key`.
   config.read_encrypted_secrets = true
 
+  # https://stackoverflow.com/questions/23180650/how-to-solve-error-missing-secret-key-base-for-production-environment-rai
+  config.secret_key_base = ENV["SECRET_KEY_BASE"]
+
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'sources/index'
+
+  get 'greeter/hello'
+  get 'greeter/goodbye'
+
+  root 'greeter#hello'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
